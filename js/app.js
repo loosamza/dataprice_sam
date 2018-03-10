aa3715e6907e5c248cc6fd323e966b13b9be3475
scOnTz <piyapatiii37@gmail.com>
2018-03-11 03:55:36 +0700

    Finish BX Tab

--- js/app.js
+++ js/app.js
@@ -2,26 +2,26 @@ var app = angular.module('app', ['ngSanitize', 'ngResource', 'ngMaterial', 'md.d
 
 
 // --------------- Factory ------------------
-app.factory('bxFactory', function ($http) {
+app.factory('bxFactory', ($http) => {
 
-    var nodeUrl = 'http://localhost:3000/getData';
+    var nodeUrl = 'https://lit-stream-19867.herokuapp.com/getData/';
     var url = 'https://bx.in.th/api/';
 
     var factory = {};
 
-    factory.getMarketData = function () {
+    factory.getMarketData = () => {
         return $http.post(nodeUrl, {url: url});
     }
 
-    factory.getCurrencyPairings = function () {
+    factory.getCurrencyPairings = () => {
         return $http.post(nodeUrl, {url: url + 'pairing/'})
     }
 
-    factory.getOrderBook = function (pairingId) {
+    factory.getOrderBook = (pairingId) => {
         return $http.post(nodeUrl, {url: url + 'orderbook/?pairing=' + pairingId});
     }
 
-    factory.getRecentTrades = function (pairingId) {
+    factory.getRecentTrades = (pairingId) => {
         return $http.post(nodeUrl, {url: url + 'trade/?pairing=' + pairingId})
     }
 
@@ -29,13 +29,31 @@ app.factory('bxFactory', function ($http) {
 
 });
 
+// -------------- Custom filter ----------------
+app.filter('removeText', () => {
+    return (input) => {
+        input = input || '';
+        var out = '';
+        out = input.replace('THB', '');
 
-app.controller('mainController', function (bxFactory, $scope, $window) {
+        return out.trim();
+    }
+})
 
+
+app.controller('mainController', (bxFactory, $scope, $timeout) => {
+
+    $scope.counter = 0;
+    $scope.showmore = false;
+    $scope.progress = 0;
+    $scope.loading = true;
+    $scope.quantity = {};
+    $scope.quantity.value = 5;
     $scope.marketDataList = [];
-    $scope.orderBook = {};
-    //Your $http request
-    bxFactory.getMarketData().then(function (response) {
+    $scope.recentTrades = {};
+
+
+    bxFactory.getMarketData().then((response) => {
         // console.log(response.data);
         for (var key in response.data) {
             if (response.data[key].primary_currency === 'THB') {
@@ -43,16 +61,51 @@ app.controller('mainController', function (bxFactory, $scope, $window) {
             }
         }
 
-        $scope.marketDataList.forEach(function (value) {
-            bxFactory.getOrderBook(value.pairing_id).then(function (currencyRes) {
-                $scope.orderBook[value.secondary_currency] = [];
-                $scope.orderBook[value.secondary_currency].push(currencyRes.data);
+        $scope.promise = $scope.getBxRecentTrades();
 
-            })
-        });
-        console.log($scope.marketDataList);
-        console.log($scope.orderBook);
+
     });
+    $scope.getBxRecentTrades = () => {
+        $scope.marketDataList.forEach((value) => {
+            bxFactory.getRecentTrades(value.pairing_id).then((currencyRes) => {
+                //console.log(currencyRes.data)
+                $scope.recentTrades[value.secondary_currency] = [];
+                $scope.recentTrades[value.secondary_currency].push(currencyRes.data);
+            });
+            $scope.loading = false;
+
+        });
+    }
+
+    $scope.promise = $scope.getBxRecentTrades();
+
+    $scope.onReload = () => {
+        if ($scope.progress == 100) {
+            $scope.promise = $scope.getBxRecentTrades();
+            $scope.counter = 0;
+            $scope.progress = 0;
+            timeout = $timeout($scope.onReload, 100);
+        } else {
+            $scope.counter++;
+            $scope.progress = ($scope.counter * 1) / 3;
+            // console.log($scope.progress + '%')
+            timeout = $timeout($scope.onReload, 100);
+        }
+
+    }
+
+    var timeout = $timeout($scope.onReload, 100);
+
+    //
+    // console.log($scope.marketDataList);
+    // console.log($scope.recentTrades);
+
+    $scope.getRecentTradesBidWithId = (id, key) => {
+        return _.find($scope.recentTrades[key][0], (item) => {
+            return item.order_id = id;
+        })
+
+    }
 
 
     console.log('mainController');
